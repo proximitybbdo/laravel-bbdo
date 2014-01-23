46a64be17d3f9b74b1d46a69b0daa53b80f42089
Pieter Michels <pieter@noort.be>
2014-01-23 12:36:25 +0100

    base-database+timezone

---
+++ app/config/development/database.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Development database settings.
+ */
+
+return array(
+  'connections' => array(
+    'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => '127.0.0.1',
+			'database'  => 'database',
+			'username'  => 'root',
+			'password'  => '',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+      'prefix'    => '',
+    )
+  ),
+);
+
+
---
+++ app/config/production/database.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Staging database settings.
+ */
+
+return array(
+  'connections' => array(
+    'mysql' => array(
+			'driver'    => 'mysql',
+      'host'      => 'BBDNS-DB02.bbdohosting.lcl',
+			'database'  => 'database',
+			'username'  => 'root',
+			'password'  => '',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+      'prefix'    => '',
+    )
+  ),
+);
+
+
