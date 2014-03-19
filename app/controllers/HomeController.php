b4a44d3da6c90ebb13336663e9b8f10096863beb
Pieter Michels <pieter@noort.be>
2014-03-19 16:38:34 +0100

    Update HomeController.php
    Default lang route

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -5,6 +5,10 @@ class HomeController extends BaseController
   public function __construct() {
     $this->beforeFilter('@filterRequests');
   }
+  
+  public function get_lang() {
+    return Redirect::to('/' . Config::get('app.locale'));
+  }
 
   public function get_index() {
     $this->layout->content = View::make('index', $this->data);
