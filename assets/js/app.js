f505c2841e2c248d4585f01277f83978ec88c4a6
Jeroen Bourgois <jeroenbourgois@gmail.com>
2014-05-07 16:46:53 +0200

    fix

---
+++ assets/js/app.js
@@ -0,0 +1,93 @@
+(function() {
+  var __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  if (window.BBDO == null) {
+    window.BBDO = {
+      Events: {}
+    };
+  }
+
+  BBDO.Site = (function() {
+    Site.prototype.current_page = null;
+
+    Site.prototype.$el = null;
+
+    function Site($el) {
+      this.$el = $el;
+      this.route(this.$el.data('page'));
+      this.init_events();
+      this.init_shared();
+    }
+
+    Site.prototype.init_events = function() {
+      return $(document).on('goto', this.goto);
+    };
+
+    Site.prototype.goto = function(e, page) {
+      return document.location = window.base_url + page;
+    };
+
+    Site.prototype.route = function(page) {
+      switch (page) {
+        case '':
+          return this.current_page = new BBDO.Index();
+        default:
+          return this.current_page = new BBDO.Base();
+      }
+    };
+
+    Site.prototype.init_shared = function() {};
+
+    return Site;
+
+  })();
+
+  $(document).ready(function() {
+    window.site.app = new BBDO.Site($('body'));
+    return $(document).on("click", "a[href]", function(e) {
+      var href, root, to_frame;
+      href = $(this).prop("href");
+      to_frame = $(this).prop("target") === '_blank';
+      root = window.location.protocol + "//" + window.location.host;
+      if (href.slice(0, root.length) !== root) {
+        if (!to_frame) {
+          e.preventDefault();
+        }
+        ga('send', 'event', 'outbound', 'click', href, {
+          'hitCallback': (function(_this) {
+            return function() {
+              if (!to_frame) {
+                return document.location = href;
+              }
+            };
+          })(this)
+        });
+        if (!to_frame) {
+          return false;
+        }
+      }
+    });
+  });
+
+  BBDO.Base = (function() {
+    function Base() {}
+
+    return Base;
+
+  })();
+
+  BBDO.Index = (function(_super) {
+    __extends(Index, _super);
+
+    function Index() {
+      Index.__super__.constructor.call(this);
+    }
+
+    return Index;
+
+  })(BBDO.Base);
+
+}).call(this);
+
+//# sourceMappingURL=app.js.map
---
+++ assets/js/app.src.coffee
@@ -0,0 +1,63 @@
+window.BBDO ?= {
+  Events: {},
+}
+
+class BBDO.Site
+  current_page: null
+  $el: null
+
+  constructor: (@$el) ->
+    @route @$el.data('page')
+
+    @init_events()
+    @init_shared()
+
+  init_events: ->
+    $(document).on 'goto', @goto
+
+  goto: (e, page) ->
+    document.location = window.base_url + page
+
+  route: (page) ->
+    switch page
+      when ''
+        @current_page = new BBDO.Index()
+      else
+        @current_page = new BBDO.Base()
+
+  init_shared: ->
+
+$(document).ready ->
+  window.site.app = new BBDO.Site $('body')
+
+  # tracking
+  $(document).on "click", "a[href]", (e) ->
+    href = $(this).prop "href"
+    to_frame = $(this).prop("target") == '_blank'
+    root = window.location.protocol + "//" + window.location.host
+
+    if href.slice(0, root.length) != root
+      e.preventDefault() unless to_frame
+
+      ga(
+        'send',
+        'event',
+        'outbound',
+        'click',
+        href,
+        {
+          'hitCallback': () =>
+            unless to_frame
+              document.location = href
+        }
+      )
+
+      false unless to_frame
+
+class BBDO.Base
+  constructor: () ->
+    # niets
+
+class BBDO.Index extends BBDO.Base
+  constructor: () ->
+    super()
